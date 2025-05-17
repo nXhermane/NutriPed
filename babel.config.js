abfe397306f110572548ef9a5ef6bdf2b7fbf1e9
nXhermane <nxhermane@gmail.com>
2025-05-17 14:20:20 +0100

    Initilize NutriPed

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = function (api) {
+  api.cache(true);
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+
+    plugins: [
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }]
+      , ["module-resolver", {
+        root: ["./"],
+
+        alias: {
+          "@": "./",
+          "tailwind.config": "./tailwind.config.js"
+        }
+      }]]
+  };
+};
