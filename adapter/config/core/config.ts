cc8bf53b3c9a3bdbb45aacb50fc56cb7ab60f99c
nXhermane <nxhermane@gmail.com>
2025-07-10 04:58:21 +0100

    feat: Enhance CalculationTools with TabView navigation and improve UI components refactor: Remove GrowthReferenceTableHeader and integrate TopTabBarItem for better navigation feat: Add delete and export functionality to AnthropometricCalculatorHistory style: Update UI elements for better visual consistency across components chore: Add xlsx dependency for spreadsheet functionality

--- adapter/config/core/config.ts
+++ adapter/config/core/config.ts
@@ -1,5 +1,6 @@
-export const version = "@v0.0.11-beta";
+export const version = "@v0.0.12-beta";
 export const CORE_CONFIG = {
   pediatricDataZipUrl: `https://cdn.jsdelivr.net/gh/nxh-labs/Pediatric_Software_Data_Extraction${version}/dist/PediatricSoftWareData.zip`,
   protocolePdfUrl: `https://cdn.jsdelivr.net/gh/nxh-labs/Pediatric_Software_Data_Extraction${version}/dist/protocole.pdf`,
+  anthropometricCalculatorResultExportTemplateUrl: `https://cdn.jsdelivr.net/gh/nxh-labs/Pediatric_Software_Data_Extraction${version}/dist/export_anthropometric_result_template.xlsx.pdf`
 } as const;
