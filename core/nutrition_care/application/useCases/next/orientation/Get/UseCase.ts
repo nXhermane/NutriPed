f43ea144c4a7fc206f66122c53be076bbdd185ea
nXhermane <142807581+nXhermane@users.noreply.github.com>
2025-09-03 18:57:33 +0100

    Update core/nutrition_care/application/useCases/next/orientation/Get/UseCase.ts

--- core/nutrition_care/application/useCases/next/orientation/Get/UseCase.ts
+++ core/nutrition_care/application/useCases/next/orientation/Get/UseCase.ts
@@ -52,7 +52,7 @@ export class GetOrientationReferenceUseCase
         return left(Result.fail("The Orientation Ref not found."));
       }
 
-      return right(Result.ok(orientationRefs.map(this.mapper.toResponse)));
+      return right(Result.ok(orientationRefs.map(entity => this.mapper.toResponse(entity))));
     } catch (e: unknown) {
       return left(handleError(e));
     }
