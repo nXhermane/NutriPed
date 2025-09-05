d13bb6f403b1705d1336147fc53fa511980c6ace
nXhermane <nxhermane@gmail.com>
2025-09-05 20:27:44 +0100

    feat(chart): Refactor GrowthReferenceChartBottomSheet to use BottomSheetModal
    This commit refactors the GrowthReferenceChartBottomSheet component to utilize the BottomSheetModal from the @gorhom/bottom-sheet library, enhancing the modal presentation and interaction. Key changes include:
    - Introduction of a modal trigger component for better separation of concerns.
    - Implementation of back navigation handling to close the modal or navigate back.
    - Updated styling and structure for improved user experience.
    
    Additionally, minor adjustments were made to the GrowthReferenceChart component for consistency in styling.

--- components/pages/chart_detail/GrowthReferecenceChartBottomSheet.tsx
+++ components/pages/chart_detail/GrowthReferecenceChartBottomSheet.tsx
@@ -19,46 +19,56 @@ import {
   GrowthReferenceChart,
   GrowthReferenceChartProps,
 } from "./GrowthReferenceChart";
-import { BlurView } from "expo-blur";
 import { FakeBlur } from "@/components/custom";
+import { Pressable } from "@/components/ui/pressable";
+import {
+  BottomSheetModal,
+  BottomSheetScrollView as GorhomBottomSheetScrollView,
+} from "@gorhom/bottom-sheet";
+import { BackHandler } from "react-native";
+import { router } from "expo-router";
 
 export interface GrowthReferenceChartBottomSheetProps
   extends GrowthReferenceChartProps {}
-export const GrowthReferenceChartBottomSheet: React.FC<GrowthReferenceChartBottomSheetProps> =
+
+const GrowthReferenceChartModal: React.FC<GrowthReferenceChartBottomSheetProps> =
   React.memo(({ ...props }) => {
     const { colorMode } = useUI();
+    const bottomSheetModalRef = React.useRef<BottomSheetModal>(null);
+    const [modalIsVisible, setModalIsVisible] = React.useState<boolean>(false);
+    const handlePress = React.useCallback(() => {
+      bottomSheetModalRef.current?.present();
+      setModalIsVisible(!modalIsVisible);
+    }, [modalIsVisible]);
+    React.useEffect(() => {
+      const backAction = () => {
+        if (modalIsVisible) {
+          bottomSheetModalRef.current?.close();
+        } else {
+          if (router.canGoBack()) {
+            router.back();
+          }
+        }
+        return true;
+      };
+
+      const backHandler = BackHandler.addEventListener(
+        "hardwareBackPress",
+        backAction
+      );
+
+      return () => backHandler.remove();
+    }, [modalIsVisible]);
 
     return (
-      <BottomSheet>
-        <HStack className="absolute bottom-0">
-          <FakeBlur
-            // experimentalBlurMethod="dimezisBlurView"
-            // intensity={50}
-            // tint={colorMode}
-            className="h-full w-full overflow-hidden rounded-t-2xl p-4"
-          >
-            <BottomSheetTrigger>
-              <HStack className="h-v-10 items-center justify-center gap-4 rounded-xl border-[0.1px] border-primary-c_light">
-                <VStack className="gap-0">
-                  <Icon as={ChevronUp} className="text-primary-c_light" />
-                  <Icon as={ChevronUp} className="text-primary-c_light/70" />
-                </VStack>
-                <Text className="font-h4 font-medium text-primary-c_light">
-                  Afficher la courbe
-                </Text>
-                <VStack className="gap-0">
-                  <Icon as={ChevronUp} className="text-primary-c_light" />
-                  <Icon as={ChevronUp} className="text-primary-c_light/70" />
-                </VStack>
-              </HStack>
-            </BottomSheetTrigger>
-          </FakeBlur>
-        </HStack>
-
-        <BottomSheetPortal
-          snapPoints={["75%", "90%"]}
-          enableContentPanningGesture={false}
-          backdropComponent={BottomSheetBackdrop}
+      <React.Fragment>
+        <GrowthReferenceChartModalTrigger onPress={handlePress} />
+        <BottomSheetModal
+          onDismiss={() => {
+            setModalIsVisible(false);
+          }}
+          snapPoints={["85%", "90%", "95%"]}
+          ref={bottomSheetModalRef}
           handleIndicatorStyle={{
             backgroundColor:
               colorMode === "light" ? colors.gray["300"] : colors.gray["500"],
@@ -72,17 +82,44 @@ export const GrowthReferenceChartBottomSheet: React.FC<GrowthReferenceChartBotto
               />
             );
           }}
+          enablePanDownToClose={true}
+          enableDynamicSizing={false}
+          enableContentPanningGesture={false}
         >
-          <BottomSheetContent className="bg-background-secondary">
-            <BottomSheetScrollView
-              className={"h-[90vh]"}
-              contentContainerClassName={""}
-              showsVerticalScrollIndicator={false}
-            >
+          <VStack className="flex-1 bg-background-primary">
+            <GorhomBottomSheetScrollView showsVerticalScrollIndicator={false}>
               <GrowthReferenceChart {...props} />
-            </BottomSheetScrollView>
-          </BottomSheetContent>
-        </BottomSheetPortal>
-      </BottomSheet>
+            </GorhomBottomSheetScrollView>
+          </VStack>
+        </BottomSheetModal>
+      </React.Fragment>
     );
   });
+export interface GrowthReferenceChartModalTriggerProps {
+  onPress: () => void;
+}
+export const GrowthReferenceChartModalTrigger: React.FC<GrowthReferenceChartModalTriggerProps> =
+  React.memo(({ onPress }) => {
+    return (
+      <HStack className="absolute bottom-0">
+        <FakeBlur className="h-full w-full overflow-hidden rounded-t-2xl p-4">
+          <Pressable onPress={onPress}>
+            <HStack className="h-v-10 items-center justify-center gap-4 rounded-xl border-[0.1px] border-primary-c_light">
+              <VStack className="gap-0">
+                <Icon as={ChevronUp} className="text-primary-c_light" />
+                <Icon as={ChevronUp} className="text-primary-c_light/70" />
+              </VStack>
+              <Text className="font-h4 font-medium text-primary-c_light">
+                Afficher la courbe
+              </Text>
+              <VStack className="gap-0">
+                <Icon as={ChevronUp} className="text-primary-c_light" />
+                <Icon as={ChevronUp} className="text-primary-c_light/70" />
+              </VStack>
+            </HStack>
+          </Pressable>
+        </FakeBlur>
+      </HStack>
+    );
+  });
+export const GrowthReferenceChartBottomSheet = GrowthReferenceChartModal;
